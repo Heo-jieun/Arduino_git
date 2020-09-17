ae057ba6589d8509ab4ec334cef52e3332fcd619
Heo-jieun <jieunheo192@gmail.com>
2020-09-17 13:59:15 +0900

    1초동안 LED의 켜짐유지 --> 이후1초에 5번 깜빡이기 05_practice

---
+++ 05_practice_2/05_practice_2.ino
@@ -0,0 +1,34 @@
+#define PIN_LED 7
+unsigned int count, state ;
+
+void setup() {
+  pinMode(PIN_LED, OUTPUT);
+  Serial.begin(115200);
+  count = state = 0;
+  digitalWrite(PIN_LED,state); // turn on LED
+ 
+}
+
+void loop() {
+  state = toggle_state(count);
+  digitalWrite(PIN_LED, state);
+  count += 1 ;
+  delay(100); // wait for 200 milliseconds  
+}
+int toggle_state(int count){
+  if(count < 11){
+    state = 0;
+  }
+  else if( 11 <= count && count < 21 && count%2==0){
+    state = 0;
+  }
+  else if( 11 <= count && count < 21 && count%2==1){
+    state = 1;
+  }
+  else{
+    state = 1;
+  }
+  return state;
+  while(1){
+    ;
+  }
